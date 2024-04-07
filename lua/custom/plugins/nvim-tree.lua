808ae9595f00df28d804b22f7737e3f8270e1985
MR_Stick <stoica.demeuleneir@gmail.com>
2024-04-07 02:52:46 +0200

    fixed nvimtree open in current dir

---
+++ lua/custom/plugins/nvim-tree.lua
@@ -0,0 +1,22 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+return {
+  {
+    'nvim-tree/nvim-tree.lua',
+    Lazy = false,
+    requires = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('nvim-tree').setup {
+        respect_buf_cwd = true, -- Ensure nvim-tree respects current buffer's directory
+        hijack_cursor = true, -- Hijack cursor to the file being edited
+        view = {
+          width = 20, -- Set width of nvim-tree window
+        },
+      }
+    end,
+  },
+  -- Keybind to toggle nvim-tree
+  -- vim.keymap.set('n', '-', ':NvimTreeToggle<cr>'),
+  vim.keymap.set('n', '-', ':NvimTreeFindFileToggle!<cr>'),
+}
